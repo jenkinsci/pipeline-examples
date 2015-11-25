579f6403431bb2254f8af1ccee791232fc0b83f1
Brian Johnson <b2jrock@gmail.com>
2015-11-24 21:44:52 -0800

    Slack notification example

---
+++ slacknotify/slackNotify.groovy
@@ -0,0 +1,12 @@
+import groovy.json.JsonOutput
+// Add whichever params you think you'd most want to have
+// replace the slackURL below with the hook url provided by
+// slack when you configure the webhook
+def notifySlack(text, channel) {
+    def slackURL = 'https://hooks.slack.com/services/xxxxxxx/yyyyyyyy/zzzzzzzzzz'
+    def payload = JsonOutput.toJson([text      : text,
+                                     channel   : channel,
+                                     username  : "jenkins",
+                                     icon_emoji: ":jenkins:"])
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+}
