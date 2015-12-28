4c40f6610d6a44c02899592a4169f37f22e612d7
Ohad Basan <ohad.basan@ironsrc.com>
2015-12-28 18:00:39 +0200

    - add README.md - add inline documentation

---
+++ workflow-examples/jobs-in-parallel/README.md
@@ -0,0 +1,4 @@
+This code snippet will run the same job multiple times in parallel 
+a usecase of that is, for example, a system test or load test that requires several workers with heavy i/o or compute.
+it allows you to run each worker on a different machine to distribute the i/o or compute
+
--- workflow-examples/jobs-in-parallel/jobs_in_parallel.groovy
+++ workflow-examples/jobs-in-parallel/jobs_in_parallel.groovy
@@ -1,9 +1,15 @@
+// in this array we'll place the jobs that we wish to run
 def branches = [:]
+
+//running the job 4 times concurrently
+//the dummy parameter is for preventing mutation of the parameter before the execution of the closure.
+//we have to assign it outside the closure or it will run the job multiple times with the same paraemter "4"
+//and jenkins will unite them into a single run of the job 
+
 for (int i = 0; i < 4; i++) {
-  def which = "${i}" 
   branches["branch${i}"] = {
-    build job: 'test_jobs', parameters: [[$class: 'StringParameterValue', name: 'queue', value:   
-      'test_praam'], [$class: 'StringParameterValue', name:'dummy', value: which]]
+    build job: 'test_jobs', parameters: [[$class: 'StringParameterValue', name: 'param1', value:   
+      'test_param'], [$class: 'StringParameterValue', name:'dummy', value: "${i}"]]
     }
 } 
 parallel branches
