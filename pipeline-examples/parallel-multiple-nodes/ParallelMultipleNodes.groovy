d672eeaa891d0f5a555e8641d580e389295d7469
Owen Mehegan <owen@brightroll.com>
2016-06-28 16:32:07 -0700

    Comment that explains the builders map

--- pipeline-examples/parallel-multiple-nodes/ParallelMultipleNodes.groovy
+++ pipeline-examples/parallel-multiple-nodes/ParallelMultipleNodes.groovy
@@ -2,6 +2,7 @@ def labels = ['precise', 'trusty'] // labels for Jenkins node types we will buil
 def builders = [:]
 for (x in labels) {
     def label = x
+    // Create a map to pass in to the 'parallel' step so we can fire all the builds at once
     builders[label] = {
       node(label) {
 	// build steps that should happen on all nodes go here
