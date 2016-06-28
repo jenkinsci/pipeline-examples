7607dfacaf4c0da6417ef8a0eb6996300d945512
Owen Mehegan <owen@brightroll.com>
2016-06-28 16:51:49 -0700

    Add a comment explaining the variable binding

--- pipeline-examples/parallel-multiple-nodes/ParallelMultipleNodes.groovy
+++ pipeline-examples/parallel-multiple-nodes/ParallelMultipleNodes.groovy
@@ -1,7 +1,8 @@
 def labels = ['precise', 'trusty'] // labels for Jenkins node types we will build on
 def builders = [:]
 for (x in labels) {
-    def label = x
+    def label = x // Need to bind the label variable before the closure - can't do 'for (label in labels)'
+
     // Create a map to pass in to the 'parallel' step so we can fire all the builds at once
     builders[label] = {
       node(label) {
