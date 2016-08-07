d3674ff7c2f0974a30602e8d5ac575b21f221ea0
Kurt Madel <kmadel@mac.com>
2016-08-07 09:49:22 -0400

    Update access-repo-information.groovy
    Added description

--- pipeline-examples/github-org-plugin/access-repo-information.groovy
+++ pipeline-examples/github-org-plugin/access-repo-information.groovy
@@ -1,6 +1,10 @@
 #!groovy
 
     // github-organization-plugin jobs are named as 'org/repo/branch'
+    // we don't want to assume that the github-organization job is at the top-level
+    // instead we get the total number of tokens (size) 
+    // and work back from the branch level Pipeline job where this would actually be run
+    // Note: that branch job is at -1 because Java uses zero-based indexing
     tokens = "${env.JOB_NAME}".tokenize('/')
     org = tokens[tokens.size()-3]
     repo = tokens[tokens.size()-2]
