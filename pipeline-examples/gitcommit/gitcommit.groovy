51aa88c61b9c4c36b85395b8b89feebdc60f26d4
Mike Kobit <mkobit@gmail.com>
2016-08-08 20:19:59 -0500

    Switch Git commit example to using `sh` step and using `returnStdout`

--- pipeline-examples/gitcommit/gitcommit.groovy
+++ pipeline-examples/gitcommit/gitcommit.groovy
@@ -2,14 +2,6 @@
 // checked out your sources on the slave. A 'git' executable
 // must be available.
 // Most typical, if you're not cloning into a sub directory
-sh('git rev-parse HEAD > GIT_COMMIT')
-git_commit=readFile('GIT_COMMIT')
+gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 // short SHA, possibly better for chat notifications, etc.
-short_commit=git_commit.take(6)
-
-//create a GIT_COMMIT file in workspace and read back into a string in Pipeline
-// If you have your sources checked out in a 'src' subdir
-sh('cd src && git rev-parse HEAD > GIT_COMMIT')
-git_commit=readFile('src/GIT_COMMIT')
-// short SHA, possibly better for chat notifications, etc.
-short_commit=git_commit.take(6)
+shortCommit = gitCommit.take(6)
