84a85dbd177f9f43145de4a4f171696d4c2cbce4
eyalbe4 <00Rotshild00>
2016-12-28 19:13:06 +0200

    Artifactory examples.

---
+++ pipeline-examples/artifactory-generic-upload-download/artifactoryGenericUploadDownload.groovy
@@ -0,0 +1,45 @@
+node {
+    git url: 'https://github.com/jfrogdev/project-examples.git'
+
+    // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
+    def server = Artifactory.server "SERVER_ID"
+
+    // Read the upload spec which was downloaded from github.
+    def uploadSpec =
+            '''{
+            "files": [
+                {
+                    "pattern": "resources/Kermit.*",
+                    "target": "libs-snapshot-local",
+                    "props": "p1=v1;p2=v2"
+                },
+                {
+                    "pattern": "resources/Frogger.*",
+                    "target": "libs-snapshot-local"
+                }
+            ]
+        }'''
+
+    // Upload to Artifactory.
+    def buildInfo1 = server.upload spec: uploadSpec
+
+    // Read the upload spec and upload files to Artifactory.
+    def downloadSpec =
+        '''{
+            "files": [
+                {
+                    "pattern": "libs-snapshot-local/*.zip",
+                    "target": "dependencies/,
+                    "props": "p1=v1;p2=v2"
+                }
+            ]
+        }'''
+
+    def buildInfo2 = server.download spec: downloadSpec
+
+    // Merge the upload and download build-info objects.
+    buildInfo1.append buildInfo2
+
+    // Publish the build to Artifactory
+    server.publishBuildInfo buildInfo1
+}
---
+++ pipeline-examples/artifactory-maven-build/artifactoryMavenBuild.groovy
@@ -0,0 +1,22 @@
+node {
+    // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
+    def server = Artifactory.server "SERVER_ID"
+    // Create an Artifactory Maven instance.
+    def rtMaven = Artifactory.newMavenBuild()
+
+    stage 'Clone sources'
+    git url: 'https://github.com/jfrogdev/project-examples.git'
+
+    stage 'Artifactory configuration'
+    // Tool name from Jenkins configuration
+    rtMaven.tool = "Maven-3.3.9"
+    // Set Artifactory repositories for dependencies resolution and artifacts deployment.
+    rtMaven.deployer releaseRepo:'libs-release-local', snapshotRepo:'libs-snapshot-local', server: server
+    rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
+
+    stage 'Exec Maven'
+    def buildInfo = rtMaven.run pom: 'maven-example/pom.xml', goals: 'clean install'
+
+    stage 'Publish build info'
+    server.publishBuildInfo buildInfo
+}
