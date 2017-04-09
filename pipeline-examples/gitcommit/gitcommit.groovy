42acfe747aeb5637882d95f2a686321013a81d01
Ramanathan.M <ramanathan.muthiah@fireeye.com>
2017-04-09 13:49:59 +0530

    retrieve changeset associated with commit

--- pipeline-examples/gitcommit/gitcommit.groovy
+++ pipeline-examples/gitcommit/gitcommit.groovy
@@ -5,3 +5,7 @@
 gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 // short SHA, possibly better for chat notifications, etc.
 shortCommit = gitCommit.take(6)
+// Along with SHA-1 id of the commit, it will be useful to retrieve changeset associated with that commit
+// This command results in output indicating several one of these and the affected files:
+// Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R)
+commitChangeset = sh(returnStdout: true, script: 'git diff-tree --no-commit-id --name-status -r HEAD').trim()
