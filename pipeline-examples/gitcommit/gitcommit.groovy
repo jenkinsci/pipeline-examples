960779c662021c0cf779d146112d6a09bc171e44
Julie Ng <uxjulie@gmail.com>
2017-09-11 13:13:13 +0200

    Use standard git log command for short commit sha
    - Long-lived repositories may need more than 6 leading characters to be unique. 
    - Use a standard git command to cover all use cases.

--- pipeline-examples/gitcommit/gitcommit.groovy
+++ pipeline-examples/gitcommit/gitcommit.groovy
@@ -2,6 +2,4 @@
 // checked out your sources on the agent. A 'git' executable
 // must be available.
 // Most typical, if you're not cloning into a sub directory
-gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-// short SHA, possibly better for chat notifications, etc.
-shortCommit = gitCommit.take(6)
+shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
