72092ca5a3e20ce2511d8fafd73457444db1fbde
Jakub Bujny <jakub.bujny@ac-project.net>
2017-11-13 19:15:34 +0100

    change mouting file from host to less confusing

--- pipeline-examples/build-in-docker-container/BuildInDockerContainer.groovy
+++ pipeline-examples/build-in-docker-container/BuildInDockerContainer.groovy
@@ -5,8 +5,8 @@ node {
     stage("build") {
         writeFile file: "test.txt", text: "test"
         docker.withRegistry("https://my-private-registry.intranet.net", 'docker-registry-credentials') {
-            docker.image("my-private-registry.intranet.net/tool/golang").inside("-v /mount_something_from_host:/mounted") { c ->
-                sh 'cat /mounted' // we can mount any file from host
+            docker.image("my-private-registry.intranet.net/tool/golang").inside("-v /home/jenkins/foo.txt:/foo.txt") { c ->
+                sh 'cat /foo.txt' // we can mount any file from host
                 sh 'cat test.txt' // we can access files from workspace
                 sh 'echo "modified-inside-container" > test.txt' // we can modify files in workspace
                 sh 'go build' // we can run command from docker image
