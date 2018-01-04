5e3bf8d6c2d4b527f70dd19adb5e514a8e028840
lvthillo <lorenz.vanthillo@outlook.com>
2018-01-04 22:01:11 +0100

    add whenBackToNormalMail.groovy example

---
+++ declarative-examples/simple-examples/whenBackToNormalMail.groovy
@@ -0,0 +1,35 @@
+pipeline {
+  /*
+   * This example shows how you can send "back to normal" mails.
+   * The post section checks if the build was failed (failure).
+   * If the build was failed an email is sent.
+   * If the build was successfull AND the previous build was failed,
+   * a "back to normal" mail is sent.
+   */
+
+  agent any
+
+  stages {
+    stage("Hello") {
+      steps {
+        sh 'echo "Hello"'
+      }
+    }
+  }
+
+  post {
+    failure {
+      mail to: 'user@mail.com',
+      subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+      body: "Build failed: ${env.BUILD_URL}"
+    }
+
+    success {
+      if (currentBuild.previousBuild != null && currentBuild.previousBuild.result != 'SUCCESS') {
+        mail to: 'user@mail.com',
+        subject: "Pipeline Success: ${currentBuild.fullDisplayName}",
+        body: "Build is back to normal (success): ${env.BUILD_URL}"     
+      }           
+    }
+  }
+}
