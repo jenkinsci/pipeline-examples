77d8613b51ab3c906d2b467a562d8061f5b6a35f
Ramanathan.M <rus.cahimb@gmail.com>
2018-02-20 20:35:35 +0530

    fetch port number of the running container and port on the host

---
+++ declarative-examples/simple-examples/dockercontainerPort.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent any
+
+    options {
+        timestamps()
+    }
+
+    environment {
+        IMAGE = "custom-tutum"
+    }
+
+    stages {
+        stage('prep') {
+            steps {
+                script {
+                    env.GIT_HASH = sh(
+                        script: "git show --oneline | head -1 | cut -d' ' -f1",
+                        returnStdout: true
+                    ).trim()
+                }
+            }
+        }
+        stage('build') {
+            steps {
+                script {
+                    image = docker.build("${IMAGE}")
+                    println "Newly generated image, " + image.id
+                }
+            }
+        }
+        stage('test') {
+            steps {
+                script {
+                    // https://hub.docker.com/r/tutum/hello-world/
+                    def container = image.run('-p 80')
+                    def contport = container.port(80)
+                    def resp = sh(returnStdout: true,
+                                        script: """
+                                                set -x
+                                                curl -w "%{http_code}" -o /dev/null -s \
+                                                http://\"${contport}\"
+                                                """
+                                        ).trim()
+                    if ( resp == "200" ) {
+                        println "tutum hello world is alive and kicking!"
+                        currentBuild.result = "SUCCESS"
+                    } else {
+                        println "Humans are mortals."
+                        currentBuild.result = "FAILURE"
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
