ee3fdf235a27e46459e39dbfa170d135788ded40
Ramanathan.M <rus.cahimb@gmail.com>
2018-04-09 20:58:48 +0530

    code trimmed and, cleaned off extra file from the previous commit

--- declarative-examples/simple-examples/dockercontainerPort.groovy
+++ declarative-examples/simple-examples/dockercontainerPort.groovy
@@ -1,15 +1,21 @@
 pipeline {
-
     environment {
-        IMAGE = "custom-tutum"
+        IMAGE = "docker-repository/custom-tutum"
     }
 
-    agent 'node-where-docker-commands-can-be-run'
+    // build agent where docker commands can be run
+    agent 'docker-node'
 
     stages {
         stage('build') {
             steps {
                 script {
+                    // Dockerfile should be stored at the dir level same as that of Jenkinsfile
+                    // Contents of Dockerfile,
+                    /*
+                        FROM tutum/hello-world
+                        EXPOSE 80
+                    */
                     image = docker.build("${IMAGE}")
                     println "Newly generated image, " + image.id
                 }
@@ -19,6 +25,7 @@ pipeline {
             steps {
                 script {
                     // https://hub.docker.com/r/tutum/hello-world/
+                    // build agent should have 'curl' installed.
                     def container = image.run('-p 80')
                     def contport = container.port(80)
                     def resp = sh(returnStdout: true,
@@ -29,10 +36,10 @@ pipeline {
                                                 """
                                         ).trim()
                     if ( resp == "200" ) {
-                        println "tutum hello world is alive and kicking!"
+                        println "tutum hello world app is alive and kicking!"
                         currentBuild.result = "SUCCESS"
                     } else {
-                        println "Humans are mortals."
+                        println "tutum hello world app isn't reachable!!!"
                         currentBuild.result = "FAILURE"
                     }
                 }
