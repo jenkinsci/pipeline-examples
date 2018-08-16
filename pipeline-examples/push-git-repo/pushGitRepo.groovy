35b6640858f80881f1453e6465bf03f08c397193
hellojukay <lic@didapinche.com>
2018-08-16 11:12:31 +0800

    fix push-git-repo error, username and passwd should be urlencode

--- pipeline-examples/push-git-repo/pushGitRepo.groovy
+++ pipeline-examples/push-git-repo/pushGitRepo.groovy
@@ -6,8 +6,10 @@
 // credentialsId here is the credentials you have set up in Jenkins for pushing
 // to that repository using username and password.
 withCredentials([usernamePassword(credentialsId: 'git-pass-credentials-ID', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+    def u =  URLEncoder.encode(GIT_USERNAME,'UTF-8')
+    def p =  URLEncoder.encode(GIT_PASSWORD,'UTF-8')
     sh("git tag -a some_tag -m 'Jenkins'")
-    sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@<REPO> --tags')
+    sh('git push https://${u}:${p}@<REPO> --tags')
 }
 
 // For SSH private key authentication, try the sshagent step from the SSH Agent plugin.
